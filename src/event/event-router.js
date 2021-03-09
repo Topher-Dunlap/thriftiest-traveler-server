56cb62107051aa38a1b211f02dd1f818c590049c
topher-dunlap <thelonioustopher@yahoo.com>
2021-03-08 18:40:18 -0600

    pushing to merge branch to main

--- src/event/event-router.js
+++ src/event/event-router.js
@@ -4,9 +4,7 @@ const timeout = require('connect-timeout');
 const eventRouter = express.Router();
 
 let extractedEventData = [{test: "test"}];
-let userLocation = {
-    location: "MSPA-sky"
-};
+let userAirport = '';
 
 eventRouter
     .route('/')
@@ -22,77 +20,89 @@ eventRouter
                 eventsArray.forEach(eventData => {
                     let eventObj = {
                         title: eventData.title,
-                        category: eventData.category,
-                        country: eventData.country,
                         description: eventData.description,
                         brand_safe: eventData.brand_safe,
                         location: [eventData.location[1], eventData.location[0]],
                         eventLocationId: '',
+                        placeName: 'City Unavailable',
+                        countryName: 'Country Unavailable',
+                        price: undefined,
+                        carrier: 'Carrier unavailable',
+                        departure: {DepartureDate: 'date n/a'}
                     }
-                    extractedEventData.push(eventObj)
+                    extractedEventData.push(eventObj);
                 });
-                return extractedEventData
+                return extractedEventData;
             })
             .then(newExtractedEventData => {
-                newExtractedEventData.map((eventObj, idx) => {
-                    eventService.eventLocation(eventObj.location)
+                let idx = 0;
+                newExtractedEventData.forEach(eventObj => {
+                    eventService.eventLocationLatLong(eventObj.location)
                         .then(response => {
-                            eventObj.eventLocationId = response.data.Places[0].PlaceId
+                            eventObj.eventLocationId = response.data.Places[0].PlaceId;
+                            eventObj.placeName = response.data.Places[0].PlaceName;
+                            eventObj.countryName = response.data.Places[0].CountryName;
+                            idx++;
                             if (idx === newExtractedEventData.length - 1) {
-                                res.json(extractedEventData.filter(obj => obj.eventLocationId !== "this destination has no airports nearby"))
+                                res.json(extractedEventData.filter(obj => obj.eventLocationId !== "this destination has no airports nearby"));
                             }
                         })
                         .catch(error => {
-                            eventObj.eventLocationId = "this destination has no airports nearby"
+                            eventObj.eventLocationId = "this destination has no airports nearby";
+                            idx++;
                             if (idx === newExtractedEventData.length - 1) {
-                                res.json(extractedEventData.filter(obj => obj.eventLocationId !== "this destination has no airports nearby"))
+                                res.json(extractedEventData.filter(obj => obj.eventLocationId !== "this destination has no airports nearby"));
                             }
                         })
                 })
             })
             .catch(error => {
-                console.log("error catch: ", error)
-            })
-    })
-
-eventRouter
-    .route('/userLocation')
-    .get((req, res) => {
-        let userLocation = req.params.query;
-        eventService.eventLocation(userLocation)
-            .then(res => userLocationData.location = res.data.Places[0].PlaceId)
-            .catch(error => {
-                console.log("error catch: ", error.response.data)
+                console.log("/ error catch: ", error);
             })
     })
 
 eventRouter
     .route('/deals')
-    .get(timeout("6s"), (req, res) => {
-
-        let filteredEvents = extractedEventData.filter(obj => obj.eventLocationId !== "this destination has no airports nearby");
-
+    .get((req, res) => {
+        let filteredEvents = extractedEventData;
         new Promise((resolve, reject) => {
-            filteredEvents.map((eventInstance, idx) => {
-                eventService.flightPrices(eventInstance.eventLocationId, userLocation)
+            let idx = 0;
+            filteredEvents.forEach(eventInstance => {
+                eventService.flightPrices(eventInstance.eventLocationId, userAirport)
                     .then(eventInstance => {
-                        Object.assign(filteredEvents[idx],
-                            {price: eventInstance.data.Quotes[0].MinPrice},
-                            {direct: eventInstance.data.Quotes[0].Direct},
-                            {departure: eventInstance.data.Quotes[0].OutboundLeg},
-                            {carriersName: eventInstance.data.Carriers[0].Name},
-                        )
-                        if (idx === filteredEvents.length - 1) {
-                            // console.log("filteredEvents before: ", filteredEvents)
-                            filteredEvents = filteredEvents.filter(obj => !!obj.price);
-                            // console.log("filteredEvents: ", filteredEvents)
-                            resolve()
+                        // console.log("eventInstance: ", idx, eventInstance.data)
+                        if (eventInstance.data.Quotes.length > 0) {
+                            // console.log("idx inside API call, IF: ", idx)
+                            Object.assign(filteredEvents[idx],
+                                {price: eventInstance.data.Quotes[0].MinPrice},
+                                {departure: eventInstance.data.Quotes[0].OutboundLeg},
+                                {carriersName: eventInstance.data.Carriers[0].Name},
+                            )
+                            eventService.flightPricesConditional(idx, filteredEvents, resolve);
+                            idx++;
+                        } else {
+                            eventService.flightPricesConditional(idx, filteredEvents, resolve);
+                            idx++;
                         }
                     })
-                    .catch(error => console.log(error.response))
+                    .catch(error => console.log("/deals error catch", error));
             })
         })
-            .then(response => res.json(filteredEvents))
+            .then(response => {
+                console.log("last filteredEvents: ", filteredEvents)
+                res.json(filteredEvents)
+            })
+    })
+
+eventRouter
+    .route('/userAirport')
+    .get((req, res) => {
+        let userCity = req.query.userCity;
+        eventService.userAirportLocation(userCity)
+            .then(locationResponse => {
+                userAirport = locationResponse.data.Places[0].PlaceId;
+            })
+            .catch(error => console.log("/userAirport error catch: ", error.response.data, error.config.url));
     })
 
 module.exports = eventRouter
--- src/event/event-service.js
+++ src/event/event-service.js
@@ -15,7 +15,7 @@ const eventService = {
         return axios(config)
     },
 
-    eventLocation(input) {
+    eventLocationLatLong(input) {
         //working url example **DELETE** https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/US/USD/en-US/?id=47.260326,-1.577128-latlong
         const config = {
             method: 'get',
@@ -29,11 +29,25 @@ const eventService = {
         return axios(config)
     },
 
+    userAirportLocation(input) {
+        //working url example **DELETE** https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/US/USD/en-US/?id=47.260326,-1.577128-latlong
+        const config = {
+            method: 'get',
+            url: `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/US/USD/en-US/?query=${input}`,
+            headers: {
+                'x-rapidapi-key': '7d903548abmsh4e854e4b7bab61fp1a3ebcjsnf378b623ea95',
+                'x-rapidapi-host': 'skyscanner-skyscanner-flight-search-v1.p.rapidapi.com',
+                'useQueryString': 'true'
+            },
+        }
+        return axios(config)
+    },
+
     flightPrices(eventLocation, userLocationData) {
         //working url example **DELETE** https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2021-04-01?sort=-price&limit=1
         const config = {
             method: 'get',
-            url: `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/${userLocationData.location}/${eventLocation}/anytime?sort=-price&limit=1`,
+            url: `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/${userLocationData}/${eventLocation}/anytime?sort=-price&limit=1`,
             headers: {
                 'x-rapidapi-key': '7d903548abmsh4e854e4b7bab61fp1a3ebcjsnf378b623ea95',
                 'x-rapidapi-host': 'skyscanner-skyscanner-flight-search-v1.p.rapidapi.com',
@@ -43,30 +57,12 @@ const eventService = {
         return axios(config)
     },
 
-    // locationAddedArray(filteredEvents, userLocation) {
-    //     // let eventsWithLocation = filteredEvents.map((eventInstance, idx) =>
-    //     let eventsWithLocation = filteredEvents.map(async (eventInstance, idx) =>
-    //         await eventService.flightPrices(eventInstance.eventLocationId, userLocation)
-    //             .then(eventInstance => {
-    //                 Object.assign(filteredEvents[idx],
-    //                     {price: eventInstance.data.Quotes[0].MinPrice},
-    //                     {direct: eventInstance.data.Quotes[0].Direct},
-    //                     {departure: eventInstance.data.Quotes[0].OutboundLeg},
-    //                     {carriersName: eventInstance.data.Carriers[0].Name},
-    //                 )
-    //             })
-    //             .catch(function (error) {
-    //                 console.log(error.response)
-    //             })
-    //     )
-    //     return Promise.all(eventsWithLocation)
-    //         .then(res => {
-    //             console.log("eventsWithLocation: ", eventsWithLocation)
-    //             console.log("filteredEvents res: ", JSON.stringify(res))
-    //             res.json(res)
-    //         });
-    // },
-
+    flightPricesConditional(idx, array, promiseResolve) {
+        if (idx === array.length - 1) {
+            array = array.filter(obj => !!obj.price);
+            return promiseResolve();
+        }
+    },
 
 }
 
