af5c0652a4b2b61cba5aef4d47cc998678c69c4a
topher-dunlap <thelonioustopher@yahoo.com>
2021-03-14 15:47:05 -0500

    npm installed uuid (tried)

--- src/app.js
+++ src/app.js
@@ -16,9 +16,9 @@ const app = express()
 const morganOption = (NODE_ENV === 'production')
     ? 'tiny'
     : 'common';
-app.use(cors())
 app.use(morgan(morganOption))
 app.use(helmet())
+app.use(cors())
 app.use(express.json())
 
 //CORS POLICY
