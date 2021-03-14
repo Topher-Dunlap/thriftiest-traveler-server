1e694baff5ef38415f83c3ac20a0854dc4b0d17a
topher-dunlap <thelonioustopher@yahoo.com>
2021-03-14 14:57:29 -0500

    removing some console.logs

--- src/event-deals/event-router.js
+++ src/event-deals/event-router.js
@@ -66,7 +66,7 @@ eventRouter
             })
         })
             .then(response => {
-                console.log("last filteredEvents: ", filteredEvents)
+                // console.log("last filteredEvents: ", filteredEvents)
                 res.json(filteredEvents.filter(obj => obj.price !== undefined))
             })
             .catch(error => console.log("/deals error catch", error.data));
@@ -79,7 +79,7 @@ eventRouter
         eventService.userAirportLocation(userCity)
             .then(locationResponse => {
                 userAirport = locationResponse.data.Places[0].PlaceId;
-                console.log("res.data userAirport: ", locationResponse.data.Places)
+                // console.log("res.data userAirport: ", locationResponse.data.Places)
             })
             .catch(error => console.log("/userAirport error catch: ", error.response.data, error.config.url));
     })
