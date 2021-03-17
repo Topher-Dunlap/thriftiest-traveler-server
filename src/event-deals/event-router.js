5350aa707cfdf2c13db2040d691ea4b4f7f5a0be
topher-dunlap <thelonioustopher@yahoo.com>
2021-03-17 14:54:57 -0500

    adding console.logs

--- src/event-deals/event-router.js
+++ src/event-deals/event-router.js
@@ -55,6 +55,7 @@ eventRouter
                     eventService.flightPrices(eventInstance.eventLocationId, userAirport)
                         .then(eventInstance => {
                             console.log("index: ", idx)
+                            console.log("eventInstance.data.Quotes.length: ", eventInstance.data.Quotes.length)
                             if (eventInstance.data.Quotes.length > 0) {
                                 Object.assign(filteredEvents[idx],
                                     {price: eventInstance.data.Quotes[0].MinPrice},
